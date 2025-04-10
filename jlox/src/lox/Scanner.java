2ba921d8008944d93122746886c16b61fead411c
zeckhardt <zeckhardt9@gmail.com>
2025-04-10 14:18:01 -0400

    fixed scanner for new tokens

--- jlox/src/lox/Scanner.java
+++ jlox/src/lox/Scanner.java
@@ -66,6 +66,8 @@ public class Scanner {
             case '-': addToken(MINUS); break;
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
+            case '?': addToken(QUESTION); break;
+            case ':': addToken(COLON); break;
             case '*': addToken(STAR); break;
             case '!':
                 addToken(match('=') ? BANG_EQUAL : BANG);
