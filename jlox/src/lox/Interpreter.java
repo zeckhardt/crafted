6e1ab6f8a36500ba54156f32f812f706daed488b
zeckhardt <zeckhardt9@gmail.com>
2025-04-15 14:51:04 -0400

    implement resolver

--- jlox/src/lox/Interpreter.java
+++ jlox/src/lox/Interpreter.java
@@ -1,11 +1,14 @@
 package lox;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     final Environment globals = new Environment();
     private Environment environment = new Environment();
+    private final Map<Expr, Integer> locals = new HashMap<>();
     private static class BreakException extends RuntimeException {}
 
     Interpreter() {
@@ -91,7 +94,16 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Object visitVariableExpr(Expr.Variable expr) {
-        return environment.get(expr.name);
+        return lookUpVariable(expr.name, expr);
+    }
+
+    private Object lookUpVariable(Token name, Expr expr) {
+        Integer distance = locals.get(expr);
+        if (distance != null) {
+            return environment.getAt(distance, name.lexeme);
+        } else {
+            return globals.get(name);
+        }
     }
 
     private void checkNumberOperand(Token operator, Object operand) {
@@ -138,6 +150,10 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     private void execute(Stmt stmt) {
         stmt.accept(this);
+    }
+
+    void resolve(Expr expr, int depth) {
+        locals.put(expr, depth);
     }
 
     void  executeBlock(List<Stmt> statements, Environment environment) {
@@ -228,7 +244,13 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Object visitAssignExpr(Expr.Assign expr) {
         Object value = evaluate(expr.value);
-        environment.assign(expr.name, value);
+
+        Integer distance = locals.get(expr);
+        if (distance != null) {
+            environment.assign(expr.name, value);
+        } else {
+            globals.assign(expr.name, value);
+        }
         return value;
     }
 
